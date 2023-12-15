3ea5c5d4218b98dbb71a0a9fc273b6948db382e6
EmailNotFound <twicetouch27@gmail.com>
2023-12-15 00:07:04 -0500

    submit

---
+++ Source/BobAndAlice_5_2/BobAndAliceGameSubsystem.cpp
@@ -0,0 +1,66 @@
+﻿#include "BobAndAliceGameSubsystem.h"
+
+void
+UBobAndAliceGameSubsystem::Initialize (FSubsystemCollectionBase &Collection)
+{
+	Super::Initialize (Collection);
+}
+
+void
+UBobAndAliceGameSubsystem::Deinitialize ()
+{
+	Super::Deinitialize ();
+}
+
+void
+UBobAndAliceGameSubsystem::StartCountdown (float Duration)
+{
+	CountdownTime = Duration;
+}
+
+void
+UBobAndAliceGameSubsystem::UpdateCountdown (float DeltaTime)
+{
+	if (CountdownTime > 0.0f)
+		{
+			CountdownTime -= DeltaTime;
+		}
+}
+
+float
+UBobAndAliceGameSubsystem::GetCountdownTime () const
+{
+	return CountdownTime;
+}
+
+void
+AExampleActor::BeginPlay ()
+{
+	Super::BeginPlay ();
+
+	UBobAndAliceGameSubsystem *MySubsystem
+		= GetGameInstance ()->GetSubsystem<UBobAndAliceGameSubsystem> ();
+	if (MySubsystem)
+		{
+			// Start the countdown with a 5-minute duration
+			MySubsystem->StartCountdown (300.0f);
+		}
+}
+
+void
+AExampleActor::Tick (float DeltaTime)
+{
+	Super::Tick (DeltaTime);
+
+	UBobAndAliceGameSubsystem *MySubsystem
+		= GetGameInstance ()->GetSubsystem<UBobAndAliceGameSubsystem> ();
+	if (MySubsystem)
+		{
+			// Update the countdown timer
+			MySubsystem->UpdateCountdown (DeltaTime);
+
+			// For example, print the remaining time to the output log
+			UE_LOG (LogTemp, Log, TEXT ("Time remaining: %f"),
+			        MySubsystem->GetCountdownTime ());
+		}
+}
---
+++ Source/BobAndAlice_5_2/BobAndAliceGameSubsystem.h
@@ -0,0 +1,26 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BobAndAliceGameSubsystem.generated.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+
+UCLASS ()
+class BOBANDALICE_5_2_API UBobAndAliceGameSubsystem
+	: public UGameInstanceSubsystem
+{
+	GENERATED_BODY ()
+
+public:
+	virtual void Initialize (FSubsystemCollectionBase &Collection) override;
+
+	virtual void Deinitialize () override;
+
+	void StartCountDown (float duration);
+	void UpdateCountDown (float deltaTime);
+	float GetCountdownTime () const;
+
+private:
+	float CountdownTime;
+};
